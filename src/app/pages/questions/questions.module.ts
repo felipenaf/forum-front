c19a42caec19179123502583ecf17499615b4dc9
Felipe Ferreira <felipe.ferreira0192@gmail.com>
2020-05-12 08:02:09 -0300

    iniciando o questions

---
+++ src/app/pages/questions/questions.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { QuestionsRoutingModule } from './questions-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientModule } from "@angular/common/http";
+// import { AuthService } from 'src/app/@core/auth/auth.service';
+
+@NgModule({
+  declarations: [ DashboardComponent ],
+  imports: [
+    CommonModule,
+    QuestionsRoutingModule,
+    HttpClientModule,
+    NgbModule
+  ],
+  providers: [
+    // AuthService
+  ]
+})
+export class QuestionsModule { }
